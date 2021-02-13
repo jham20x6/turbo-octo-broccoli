6adc471bcbabdd3cd219fb2a164defdf29c5db24
jham20x6 <78266491+jham20x6@users.noreply.github.com>
2021-02-13 15:50:27 -0800

    cleaned and added cmts
    - try/except wrapped into addAction
    - updated dictionary format adding labels, total_time and count

--- action_tracker/Tracker.py
+++ action_tracker/Tracker.py
@@ -4,47 +4,37 @@ class ActionTracker(object):
 
     def __init__(self):
         self.actions = {}
-        #Action dictionary format { action : [ total_time, count ] }
-        #             for example { "jog"  : [ 300, 5 ] }
-        self.parsedObj = {}
-        #Format of JSON serialized string {"action":"jump", "time":100}
-        #TODO: add a check for this format as well as proper json syntax
-        self.parsingError = ""
- 
-    #validate JSON format via json.loads
-    #store either the parsed Object or the error to be returned later
-    def is_valid_json(self, jsonString):
-        try:
-            self.parsedObj = json.loads(jsonString)
-            return True
-        except ValueError as e:
-            self.parsingError = e
-            return False
-        
+        #Format of input JSON string {"action":"jump", "time":100}
+        #TODO: add a check for this format
+  
     def addAction(self, jsonString):
-        #Check if string is valid JSON.  self.parsedObj is populated if successful)
-        if self.is_valid_json(jsonString):
+        #Check if string is valid JSON.  parsedObj is populated if successful)
+        try:
+            #validate JSON format via json.loads
+            #store object on success or return error
+            parsedObj = json.loads(jsonString)
+            #TODO: Verify that format here. We at least need action and time as first two items            
+            actionKey = parsedObj['action']
             
-            #Average will later be calculated by the total time field divided by the count field.
-            actionKey = self.parsedObj['action']
             if actionKey in self.actions:
                 #Increment existing action times by the new time field
-                #self.actions[actionKey][0] = self.actions.get(actionKey)[0] + self.parsedObj['time']
-                #self.actions[actionKey][1] = self.actions.get(actionKey)[1] + 1
-                self.actions[actionKey][0] += self.parsedObj['time']
-                self.actions[actionKey][1] += 1
+                self.actions[actionKey]['total_time'] += parsedObj['time']
+                self.actions[actionKey]['count'] += 1
             else:
-                #Add new actions to the ActivityAverages list
-                self.actions[actionKey] = [self.parsedObj['time'],1]
+                #Add any new action
+                #Action dictionary format { action : { total_time: t,  count: c } }
+                self.actions[actionKey] = { "total_time":parsedObj['time'],"count":1 }
 
-        else:
+        except ValueError as e:
             #return the error from the failed json.loads() attempt
-            return(self.parsingError)
+            return(e)
 
     def getStats(self):
+        #Format of output needs to be list of actions and average times.
+        #example: [ {"action": "action1", "avg": 10}, {"action": "action2", "avg": 20 } ]
         activityStats = []
         for actionKey in self.actions:
             #build dictionary pairs for the output
             #Assumption: Average needs to only be in whole numbers, type cast as int
-            activityStats.append({"action": actionKey, "avg" : (int(self.actions[actionKey][0] / self.actions[actionKey][1])) })
+            activityStats.append({"action": actionKey, "avg" : (int(self.actions[actionKey]['total_time'] / self.actions[actionKey]['count'])) })
         return json.dumps(activityStats)
